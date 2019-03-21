4324497e2452a9b5285b9b0fd8af2d226dd8a25f
b0sya <schalaschnikov@yandex.ru>
2019-03-21 20:08:33 +0300

    TEST GIT AND JAVA

---
+++ src/main/java/ARK.java
@@ -0,0 +1,11 @@
+public class ARK {
+    static boolean valeraInARK = true;
+
+    public void checker (){
+        if (valeraInARK){
+            System.out.println("Ну и чмо же ты, Валера");
+        }else{
+            System.out.println("М - колья попа. Догадайтесь кто?");
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        TestClass.setStr("Maks is a big pussy");
+        TestClass.setStr("Maks is a big pussy\n");
         TestClass.getStr();
+        ARK puk = new ARK();
+        puk.checker();
+        ARK.valeraInARK = false;
+        puk.checker();
     }
 }
