512c19cd53a8d3dadceca3e76ed88bfce9f07633
Cagema <Cagema.erm@gmail.com>
2019-03-21 21:46:36 +0300

    21:46

--- src/main/java/ARK.java
+++ src/main/java/ARK.java
@@ -3,7 +3,7 @@ public class ARK {
 
     public void checker (){
         if (valeraInARK){
-            System.out.println("Ну и чмо же ты, Валера");
+            System.out.println("\nНу и чмо же ты, Валера");
         }else{
             System.out.println("М - колья попа. Догадайтесь кто?");
             System.out.println("PUK");
