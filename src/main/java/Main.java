42ca3aa89041d411a83e9a836924c08e2ac0bea9
Cagema <Cagema.erm@gmail.com>
2019-03-21 21:31:58 +0300

    Dfkthf xmvj

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        TestClass.setStr("Maks is a big pussy");
+        TestClass.setStr("Maks Paraside is a big pussy");
         TestClass.getStr();
     }
 }
